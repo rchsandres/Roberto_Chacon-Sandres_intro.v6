aae4fe3a3fd0dc2fbfda23f7cf14bfc05f6b76ec
rchaconsandres <rchaconsandres+github@hawk.iit.edu>
2026-05-20 23:46:05 -0500

    js added, created footer and skills

---
+++ js/index.js
@@ -0,0 +1,26 @@
+const body = document.getElementsByTagName("body")[0];
+const foot = document.createElement("footer");
+
+body.appendChild(footer);
+
+const today = new Date();
+let thisYear = today.getFullYear();
+
+const footer = document.querySelector("footer");
+
+const copyright = document.createElement("p");
+
+copyright.innerHTML = `Roberto Chacon-Sandre ${thisYear}`;
+
+footer.appendChild(copyright);
+
+const skills = ["JavaScript", "HTML", "CSS", "Github"];
+
+const skillSection = document.querySelector("skills");
+const skillsList = skillsSection.querySelector("ul");
+
+for(let i =0; i<skills.length; i++){
+    const skill = document.createElement("li");
+    skill.innerText = skills[i];
+    skillsList.appendChild(skill);
+}
